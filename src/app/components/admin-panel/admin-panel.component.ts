6bb207fd0bef501baa8622bf159ab643e954e94e
Carlos Rodriguez <carlos.rodriguez12@uptc.edu.co>
2020-12-14 21:13:31 -0500

    dashboard

--- src/app/components/admin-panel/admin-panel.component.ts
+++ src/app/components/admin-panel/admin-panel.component.ts
@@ -2,6 +2,8 @@ import { AngularFireStorage } from '@angular/fire/storage';
 import { ArticleService } from './../../services/article.service';
 import { Article } from './../../models/article';
 import { Component, OnInit } from '@angular/core';
+import { Comment } from '../../models/comment';
+import { CommentService } from 'src/app/services/comment.service';
 
 
 @Component({
@@ -15,10 +17,19 @@ export class AdminPanelComponent implements OnInit {
   description: string
   articles: Article[];
   images: string[] = [];
+  commentsList: Comment[];
+  img: string;
 
-  constructor(private articleService: ArticleService, private storage: AngularFireStorage) {
+  constructor(private articleService: ArticleService, private storage: AngularFireStorage,
+    private commentService: CommentService) {
     this.articleService.getAllArticles().subscribe(res => {
       this.articles = res['articles'];
+    });
+
+    this.commentService.getAllComments().subscribe(comments => {
+      console.log(comments);
+
+      this.commentsList = comments['comments']
     });
   }
 
@@ -28,7 +39,7 @@ export class AdminPanelComponent implements OnInit {
   publish() {
     let formatDescription = this.description.replace("\\n\\r", '<br>');
     let article = new Article(this.title, formatDescription);
-    this.images.push('assets/portada.jpg');
+    // this.images.push('assets/portada.jpg');
     article.imgs = this.images;
     this.articleService.addArticle(article).subscribe(res => {
       console.log(res);
@@ -44,15 +55,25 @@ export class AdminPanelComponent implements OnInit {
     const ref = this.storage.ref(filePath);
     const task = this.storage.upload(filePath, file)
     task.then(res => {
-      console.log(res, 'algo');
       ref.getDownloadURL().subscribe(res => {
         this.images.push(res);
-        console.log(res, 'url');
+        this.img = res;
       }),
         (error) => {
           console.log(error, 'An error has been ocurred');
         }
     });
+  }
+
+  switchComments(option: string) {
+    if (option === 'comments') {
+      document.getElementById('article').style.display = 'none';
+      document.getElementById('comments').style.display = 'block';
+      document.getElementById('comments').hidden = true;
+    } else {
+      document.getElementById('article').style.display = 'block';
+      document.getElementById('comments').style.display = 'none';
+    }
   }
 
 }
